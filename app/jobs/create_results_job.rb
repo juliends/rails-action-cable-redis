1d6ad871e44afbc7f0586ccff270b28a7441346b
julien da silva <julien.da.silva@hotmail.com>
2018-03-03 22:04:08 +0100

    checkpoint

--- app/jobs/create_results_job.rb
+++ app/jobs/create_results_job.rb
@@ -4,8 +4,11 @@ class CreateResultsJob < ApplicationJob
   def perform(id)
     10.times do 
       sleep (0..2).to_a.sample
-      r = Result.new(title: "Hello", content: "It's me <3")
-      ActionCable.server.broadcast "results-#{id}", message: "<li>#{r.title} | #{r.content}</li>"
+      @result = Result.new(title: "Hello", content: "It's me <3")
+      ActionCable.server.broadcast(
+        "results-#{id}",
+        message: ApplicationController.renderer.render(partial: "results/card", locals: { result: @result })
+      )
     end
   end
 end
